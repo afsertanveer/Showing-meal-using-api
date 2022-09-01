69dbc1f563db4c62d2c0446b2defed8ac5978a93
Abul Kawser <uiu.afser@gmail.com>
2022-09-01 09:10:25 +0600

    meal detail added

--- js/meals.js
+++ js/meals.js
@@ -8,15 +8,17 @@ const displayMeals = meals =>{
     const mealContainer = document.getElementById('meal-container');
     mealContainer.innerHTML = ``;
     meals.forEach( meal =>{
-        console.log(meal);
+      
         const mealDiv = document.createElement('div');
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-            <div class="card">
-                <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+                <img src="${
+                  meal.strMealThumb
+                }" class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">${meal.strMeal}</h5>
-                    <p class="card-text">${meal.strInstructions.slice(0,200)}
+                    <p class="card-text">${meal.strInstructions.slice(0, 200)}
                     </p>
                 </div>
             </div>
@@ -32,4 +34,32 @@ const searchFood = () => {
     searchField.value = '';
 }
 
-loadMeals('a');
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayMealDetail(data.meals[0]));
+}
+
+const displayMealDetail = (meal) =>{
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML =``;
+    const  detailDiv = document.createElement('div');
+    detailDiv.classList.add('card');
+    detailDiv.innerHTML = `
+     <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions.slice(0, 200)}
+              </p>
+            </div>
+    `;
+    detailContainer.appendChild(detailDiv);
+
+}
+
+loadMeals('');
+
+
+
+
